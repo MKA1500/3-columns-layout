a6cfa23f9a8c51ae986bf0d0b0f350aa0b4cfcec
MKA1500 <maciejkabsch@gmail.com>
2017-10-24 22:39:10 +0200

    newspaper main page

---
+++ js/app.js
@@ -0,0 +1,31 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('myController', ['$scope', function ($scope) {
+    $scope.toggle = false;
+    $scope.mainContent = [
+        {
+            "title": "Maecenas tempor in lectus a fermentum",
+            "pic": "https://c1.staticflickr.com/9/8754/16211514544_080e43019a_b.jpg",
+            "lead": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel neque congue, pretium nulla non, convallis lorem. Proin convallis tellus justo, sed mattis tortor posuere a."
+        },
+        {
+            "title": "Nam vel neque congue",
+            "pic": "https://c1.staticflickr.com/1/688/20612106292_df743e31cd_b.jpg",
+            "lead": "Dolor sit amet, consectetur adipiscing elit. Nam vel neque congue, pretium nulla non, convallis lorem. Proin convallis tellus justo, sed mattis tortor posuere a."
+        },
+        {
+            "title": "Maecenas tempor in lectus a fermentum",
+            "pic": "https://c1.staticflickr.com/9/8754/16211514544_080e43019a_b.jpg",
+            "lead": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel neque congue, pretium nulla non, convallis lorem. Proin convallis tellus justo, sed mattis tortor posuere a."
+        },
+        {
+            "title": "Nam vel neque congue",
+            "pic": "https://c1.staticflickr.com/1/688/20612106292_df743e31cd_b.jpg",
+            "lead": "Dolor sit amet, consectetur adipiscing elit. Nam vel neque congue, pretium nulla non, convallis lorem. Proin convallis tellus justo, sed mattis tortor posuere a."
+        }
+    ];
+    // $scope.leftAside = [];
+    // $scope.rightAside = [];
+
+
+}]);
