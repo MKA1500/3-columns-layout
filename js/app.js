b1c932abb4828f3b824a49efdec611e25fc22250
MKA1500 <maciejkabsch@gmail.com>
2017-10-25 22:43:55 +0200

    some progress

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ myApp.controller('myController', ['$scope', function ($scope) {
     $scope.mainContent = [
         {
             "title": "Maecenas tempor in lectus a fermentum",
-            "pic": "https://c1.staticflickr.com/9/8754/16211514544_080e43019a_b.jpg",
+            "pic": "https://c2.staticflickr.com/8/7233/7314543258_9a2570a55c_b.jpg",
             "lead": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel neque congue, pretium nulla non, convallis lorem. Proin convallis tellus justo, sed mattis tortor posuere a."
         },
         {
